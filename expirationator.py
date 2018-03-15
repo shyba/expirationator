4067eeabdb38af31f048cab1a17b8c7f2a23dd94
Victor Shyba <victor1984@riseup.net>
2018-03-15 13:26:37 -0300

    py3->py3 + flask->sanic

--- expirationator.py
+++ expirationator.py
@@ -1,49 +1,53 @@
 import requests
-from flask import Flask
-from flask import render_template
+from sanic import Sanic
+from sanic import response
+from sanic_jinja2 import SanicJinja2
 from collections import OrderedDict
 import plyvel
 import json
 
 db = plyvel.DB('db/height_claim')
-app = Flask(__name__)
+app = Sanic(__name__)
+jinja = SanicJinja2(app)
 
 
 def sorted_dump(start=None, stop=None):
     dump = OrderedDict()
+    start, stop = ((x.encode() if x else None) for x in (start, stop))
     for (k, v) in db.iterator(start=start, stop=stop):
         dump[int(k)] = json.loads(v)
     return dump
 
 
 @app.route('/dump')
-def dump_it():
-    return json.dumps(sorted_dump())
+def dump_it(request):
+    return response.json(sorted_dump())
 
 
-@app.route('/dump/<int:start>/<int:stop>')
-def dump_range(start, stop):
+@app.route('/dump/<start:int>/<stop:int>')
+def dump_range(request, start, stop):
     start, stop = ('%06d' % (x) for x in (start, stop))
-    return json.dumps(sorted_dump(start, stop))
+    return response.json(sorted_dump(start, stop))
 
 
-@app.route('/accumulated/<int:stop>')
-def accumulate(stop):
+@app.route('/accumulated/<stop:int>')
+def accumulate(request, stop):
     stop = '%06d'%(stop)
     accumulated = 0
     result = {"x":[], "y":[]}
-    for k, v in sorted_dump(stop=stop).iteritems():
+    for k, v in sorted_dump(stop=stop).items():
         accumulated += len(v)
         result["x"].append(k)
         result["y"].append(accumulated)
-    return json.dumps(result)
+    return response.json(result)
 
 
 @app.route('/')
-def plot_it():
+@jinja.template('hello.html')
+def plot_it(request):
     height = max([block['Height'] for block in requests.get('https://explorer.lbry.io/api/v1/recentblocks').json()['blocks']])
-    return render_template('hello.html', height=height)
+    return {'height': height}
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, debug=True)
