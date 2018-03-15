a0cf98ddbfb0bccc3afcef49eea5803c6d11eff5
Victor Shyba <victor1984@riseup.net>
2018-03-15 15:02:34 -0300

    simplify names storage

--- expirationator.py
+++ expirationator.py
@@ -1,21 +1,24 @@
+import struct
+
 import requests
 from sanic import Sanic
 from sanic import response
 from sanic_jinja2 import SanicJinja2
 from collections import OrderedDict
 import plyvel
-import msgpack
 
 db = plyvel.DB('db/height_claim')
 app = Sanic(__name__)
 jinja = SanicJinja2(app)
+dbpacker = struct.Struct('>I40s')
 
 
 def sorted_dump(start=None, stop=None):
     dump = OrderedDict()
-    start, stop = ((x.encode() if x else None) for x in (start, stop))
-    for (k, v) in db.iterator(start=start, stop=stop):
-        dump[int(k)] = msgpack.loads(v)
+    start, stop = ((struct.pack('>I', x) if x else None) for x in (start, stop))
+    for (key, name) in db.iterator(start=start, stop=stop):
+        height, claim_id = dbpacker.unpack(key)
+        dump.setdefault(height, {})[claim_id] = name
     return dump
 
 
@@ -26,13 +29,11 @@ def dump_it(request):
 
 @app.route('/dump/<start:int>/<stop:int>')
 def dump_range(request, start, stop):
-    start, stop = ('%06d' % (x) for x in (start, stop))
     return response.json(sorted_dump(start, stop))
 
 
 @app.route('/accumulated/<stop:int>')
 def accumulate(request, stop):
-    stop = '%06d'%(stop)
     accumulated = 0
     result = {"x": [], "y": []}
     for k, v in sorted_dump(stop=stop).items():
--- reverser.py
+++ reverser.py
@@ -1,20 +1,17 @@
 # coding: utf-8
 import plyvel
-import msgpack
+import struct
 
 height_db = plyvel.DB('db/claim_height/')
 names_db = plyvel.DB('db/claim_names/')
-by_height = {}
-for (claim_id, height) in height_db:
-    value = (claim_id, names_db.get(claim_id))
-    by_height.setdefault(b'%06d' % int(height), []).append(value)
-    
 app_db = plyvel.DB('db/height_claim/', create_if_missing=True)
+
 [app_db.delete(x[0]) for x in app_db]
-for (k, v) in by_height.items():
-    with app_db.write_batch() as writer:
-        writer.put(k, msgpack.dumps(v))
-        
+with app_db.write_batch() as writer:
+    for (claim_id, height) in height_db:
+        key = struct.pack('>I40s', int(height), claim_id)
+        writer.put(key, names_db.get(claim_id))
+
 app_db.close()
 height_db.close()
 names_db.close()
